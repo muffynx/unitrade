12cf8c8770baeef91541ad3dcb72f3ab750e69db
unknown <29300@muk.ac.th>
2025-10-25 03:43:25 +0700

    first commit4

--- src/pages/Admin/Dashborad.tsx
+++ src/pages/Admin/Dashborad.tsx
@@ -66,7 +66,7 @@ export default function Dashboard() {
   const [error, setError] = useState<string | null>(null);
 
   const token = localStorage.getItem("adminToken");
-  const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
+  const API_URL = import.meta.env.VITE_API_URL || "https://unitrade2.onrender.com";
 
   const fetchDashboardData = async () => {
     setLoading(true);
